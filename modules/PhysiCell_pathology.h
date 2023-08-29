53e24fae99f3b09b17af6c912cf34b24c3e2bcfe
Daniel Bergman <danielrbergman@gmail.com>
2023-08-28 20:58:45 -0400

    cleaning up some extraneous stuff I put in while testing

--- modules/PhysiCell_pathology.h
+++ modules/PhysiCell_pathology.h
@@ -105,7 +105,6 @@ std::vector<double> transmission( std::vector<double>& incoming_light, std::vect
 std::vector<std::string> simple_cell_coloring( Cell* pCell ); // done 
 
 std::string paint_by_density_percentage( double concentration, double max_conc, double min_conc ); //done
-std::string paint_by_density_percentage_ylorrd(double concentration, double max_conc, double min_conc); // WARNING: cannot be used at the moment because SVG_plot needs it to output a std::vector<std::string> (even though only a single std::string is used to pick the color)
 
 void setup_svg_substrate_colormap(std::vector<std::string> &colormap);
 
